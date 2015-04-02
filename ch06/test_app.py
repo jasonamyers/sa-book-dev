4c66bb6ab7126bb22a3937e347ec3cef3bb44e92
Jason Myers <jason@jasonamyers.com>
2015-04-01 20:23:08 -0500

    More work
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- ch06/test_app.py
+++ ch06/test_app.py
@@ -12,55 +12,104 @@ class TestApp(unittest.TestCase):
     def setUpClass(cls):
         dal.db_init('sqlite:///:memory:')
         prep_db()
+        #self.cookie_orders = [(u'wlk001', u'cookiemon', u'111-111-1111')]
+        #self.cookie_details = [
+            #(u'wlk001', u'cookiemon', u'111-111-1111',
+                #u'dark chocolate chip', 2, Decimal('1.00')),
+            #(u'wlk001', u'cookiemon', u'111-111-1111',
+                #u'oatmeal raisin', 12, Decimal('3.00'))]
 
-    @classmethod
-    def tearDownClass(cls):
-        dal.connection.close()
+    def test_orders_by_customer_blank(self):
+        results = get_orders_by_customer('')
+        self.assertEqual(results, [])
 
-    def setUp(self):
-        self.cookie_orders = [(u'wlk001', u'cookiemon', u'111-111-1111')]
-        self.cookie_details = [
-            (u'wlk001', u'cookiemon', u'111-111-1111',
-                u'dark chocolate chip', 2, Decimal('1.00')),
-            (u'wlk001', u'cookiemon', u'111-111-1111',
-                u'oatmeal raisin', 12, Decimal('3.00'))]
+    def test_orders_by_customer_blank_shipped(self):
+        results = get_orders_by_customer('', True)
+        self.assertEqual(results, [])
 
-    def test_orders_by_customer_bad_cust_name(self):
-        results = get_orders_by_customer('bad name')
-        assert results == []
+    def test_orders_by_customer_blank_notshipped(self):
+        results = get_orders_by_customer('', False)
+        self.assertEqual(results, [])
 
-    def test_orders_by_customer(self):
-        results = get_orders_by_customer('cookiemon')
-        assert results == self.cookie_orders
+    def test_orders_by_customer_blank_details(self):
+        results = get_orders_by_customer('', details=True)
+        self.assertEqual(results, [])
 
-    def test_orders_by_customer_shipped_only(self):
-        results = get_orders_by_customer('cookiemon', True)
-        assert results == []
+    def test_orders_by_customer_blank_shipped_details(self):
+        results = get_orders_by_customer('', True, True)
+        self.assertEqual(results, [])
 
-    def test_orders_by_customer_unshipped_only(self):
-        results = get_orders_by_customer('cookiemon', False)
-        assert results == self.cookie_orders
+    def test_orders_by_customer_blank_notshipped_details(self):
+        results = get_orders_by_customer('', False, True)
+        self.assertEqual(results, [])
+    #def test_orders_by_customer_blank(self):
+        #results = get_orders_by_customer('')
+        #assert results == []
 
-    def test_orders_by_customer_with_details(self):
-        results = get_orders_by_customer('cookiemon', details=True)
-        assert results == self.cookie_details
+    #def test_orders_by_customer_blank_shipped(self):
+        #results = get_orders_by_customer('', True)
+        #assert results == []
 
-    def test_orders_by_customer_shipped_only_with_details(self):
-        results = get_orders_by_customer('cookiemon', True, True)
-        assert results == []
+    #def test_orders_by_customer_blank_notshipped(self):
+        #results = get_orders_by_customer('', False)
+        #assert results == []
 
-    def test_orders_by_customer_unshipped_only_details(self):
-        results = get_orders_by_customer('cookiemon', False, True)
-        assert results == self.cookie_details
+    #def test_orders_by_customer_blank_details(self):
+        #results = get_orders_by_customer('', details=True)
+        #assert results == []
 
-    def test_orders_by_customer_without_details(self):
-        results = get_orders_by_customer('cookiemon', details=False)
-        assert results == self.cookie_orders
+    #def test_orders_by_customer_blank_shipped_details(self):
+        #results = get_orders_by_customer('', True, True)
+        #assert results == []
 
-    def test_orders_by_customer_shipped_only_no_details(self):
-        results = get_orders_by_customer('cookiemon', True, False)
-        assert results == []
+    #def test_orders_by_customer_blank_notshipped_details(self):
+        #results = get_orders_by_customer('', False, True)
+        #assert results == []
 
-    def test_orders_by_customer_unshipped_only_no_details(self):
-        results = get_orders_by_customer('cookiemon', False, False)
-        assert results == self.cookie_orders
+    #def test_orders_by_customer_bad_cust(self):
+        #results = get_orders_by_customer('bad name')
+        #assert results == []
+
+    #def test_orders_by_customer_bad_cust_shipped(self):
+        #results = get_orders_by_customer('bad name', True)
+        #assert results == []
+
+    #def test_orders_by_customer_bad_cust_notshipped(self):
+        #results = get_orders_by_customer('bad name', False)
+        #assert results == []
+
+    #def test_orders_by_customer_bad_cust_details(self):
+        #results = get_orders_by_customer('bad name', details=True)
+        #assert results == []
+
+    #def test_orders_by_customer_bad_cust_shipped_details(self):
+        #results = get_orders_by_customer('bad name', True, True)
+        #assert results == []
+
+    #def test_orders_by_customer_bad_cust_notshipped_details(self):
+        #results = get_orders_by_customer('bad name', False, True)
+        #assert results == []
+
+    #def test_orders_by_customer(self):
+        #results = get_orders_by_customer('cookiemon')
+        #assert results == self.cookie_orders
+
+    #def test_orders_by_customer_shipped_only(self):
+        #results = get_orders_by_customer('cookiemon', True)
+        #assert results == []
+
+    #def test_orders_by_customer_unshipped_only(self):
+        #results = get_orders_by_customer('cookiemon', False)
+        #assert results == self.cookie_orders
+
+    #def test_orders_by_customer_with_details(self):
+        #results = get_orders_by_customer('cookiemon', details=True)
+        #assert results == self.cookie_details
+
+    #def test_orders_by_customer_shipped_only_with_details(self):
+        #results = get_orders_by_customer('cookiemon', True, True)
+        #assert results == []
+
+    #def test_orders_by_customer_unshipped_only_details(self):
+        #results = get_orders_by_customer('cookiemon', False, True)
+        #assert results == self.cookie_details
