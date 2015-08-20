4b320e3b1bb73edbcf5efcb75ac0b27ec93349ec
Jason Myers <jason@jasonamyers.com>
2015-08-20 09:14:22 -0400

    Ch10
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- ch10/test_mock_app.py
+++ ch10/test_mock_app.py
@@ -8,28 +8,27 @@ from app import get_orders_by_customer
 
 
 class TestApp(unittest.TestCase):
-    cookie_orders = [(u'wlk001', u'cookiemon', u'111-111-1111')]
+    cookie_orders = [(1, u'cookiemon', u'111-111-1111')]
     cookie_details = [
-        (u'wlk001', u'cookiemon', u'111-111-1111',
+        (1, u'cookiemon', u'111-111-1111',
             u'dark chocolate chip', 2, Decimal('1.00')),
-        (u'wlk001', u'cookiemon', u'111-111-1111',
+        (1, u'cookiemon', u'111-111-1111',
             u'oatmeal raisin', 12, Decimal('3.00'))]
 
-    @mock.patch('app.select')
-    @mock.patch('app.dal.connection')
+    @mock.patch('app.dal.session')
     def test_orders_by_customer_blank(self, mock_conn, mock_select):
         mock_select.return_value.select_from.return_value.where.return_value = None
         mock_conn.execute.return_value.fetchall.return_value = []
         results = get_orders_by_customer('')
         self.assertEqual(results, [])
 
-    @mock.patch('app.dal.connection')
+    @mock.patch('app.dal.session')
     def test_orders_by_customer_blank_shipped(self, mock_conn):
         mock_conn.execute.return_value.fetchall.return_value = []
         results = get_orders_by_customer('', True)
         self.assertEqual(results, [])
 
-    @mock.patch('app.dal.connection')
+    @mock.patch('app.dal.session')
     def test_orders_by_customer(self, mock_conn):
         mock_conn.execute.return_value.fetchall.return_value = self.cookie_orders
         results = get_orders_by_customer('cookiemon')
---
+++ ch12/app/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///alembictest.db')
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+Base = declarative_base()
